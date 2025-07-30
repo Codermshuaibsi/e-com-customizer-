407b5ad4b0b487b06bd803f82d2334576abfe16b
Mohd Shuaib <heyshuaib43@gmail.com>
2025-07-30 11:01:19 +0530

    cart changes

--- routers/cartRouter.js
+++ routers/cartRouter.js
@@ -4,27 +4,27 @@ const router = express.Router();
 
 // Import the required controllers and middleware functions
 const {
-fetchAllCartItem , addToCart , removeFromCart,removeAllFromCart
+    fetchAllCartItem, addToCart, removeFromCart, removeAllFromCart, updateCartQuantity
 } = require("../controllers/cartController");
 
 
-const { addToWishlist, removeFromWishlist, fetchAllWishlistItem, removeAllWislist } = require("../controllers/wishlistController");
+const { addToWishlist, removeFromWishlist, fetchAllWishlistItem, removeAllWislist, } = require("../controllers/wishlistController");
 
-const { auth  , isUser } = require("../middleware/auth");
+const { auth, isUser } = require("../middleware/auth");
 
 // ********************************************************************************************************
 //                                      cart routes
 // ********************************************************************************************************
 
-router.get("/fetchAllCartItems",auth , isUser ,  fetchAllCartItem)     
+router.get("/fetchAllCartItems", auth, isUser, fetchAllCartItem)
 
-router.get("/AllCartItems/:id",  fetchAllCartItem)      
+router.get("/AllCartItems/:id", fetchAllCartItem)
 
-router.post("/addToCart/:productId",auth , isUser ,  addToCart) 
+router.post("/addToCart/:productId", auth, isUser, addToCart)
 
-router.post('/removeFromCart/:productId' ,auth , isUser ,  removeFromCart); 
+router.post('/removeFromCart/:productId', auth, isUser, removeFromCart);
 
-router.post('/removeAllFromCart' ,auth , isUser ,  removeAllFromCart);   
+router.post('/removeAllFromCart', auth, isUser, removeAllFromCart);
 
 router.put('/updateCartQuantity/:productId', auth, isUser, updateCartQuantity);
 
@@ -33,13 +33,13 @@ router.put('/updateCartQuantity/:productId', auth, isUser, updateCartQuantity);
 //                                     wishlist routes
 //***************************************************************************************
 
-router.post('/addToWishlist/:productId' , auth , isUser ,addToWishlist);    
+router.post('/addToWishlist/:productId', auth, isUser, addToWishlist);
 
-router.delete("/removeFromWishlist/:productId" , auth , isUser , removeFromWishlist); 
+router.delete("/removeFromWishlist/:productId", auth, isUser, removeFromWishlist);
 
-router.get("/fetchAllWishlistItem" , auth , isUser , fetchAllWishlistItem);  
+router.get("/fetchAllWishlistItem", auth, isUser, fetchAllWishlistItem);
 
-router.delete("/removeAllWislist" , auth , isUser , removeAllWislist);     
+router.delete("/removeAllWislist", auth, isUser, removeAllWislist);
 
 
 module.exports = router;  
