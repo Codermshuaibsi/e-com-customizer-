1b15fab37472390a2d4c3059e013f2c83154e10b
Mohd Shuaib <heyshuaib43@gmail.com>
2025-08-05 11:00:56 +0530

    new changes

--- routers/ShippingAddressRoutes.js
+++ routers/ShippingAddressRoutes.js
@@ -10,18 +10,18 @@ const {
 } = require("../controllers/addressController");
 
 // ✅ Create a new address
-router.post("/createAddress",auth,isUser, createAddress);
+router.post("/createAddress",auth, createAddress);
 
 // ✅ Get all addresses of a user (pass userId as query param)
-router.get("/getAllAddresses",auth ,isUser , getAddresses);
+router.get("/getAllAddresses",auth , getAddresses);
 
 // ✅ Get a single address by address ID
-router.get("/getSingleAddress/:id",auth ,isUser, getSingleAddress);
+router.get("/getSingleAddress/:id",auth , getSingleAddress);
 
 // ✅ Update address by address ID
-router.put("/updateAddress/:id",auth ,isUser , updateAddress);
+router.put("/updateAddress/:id",auth  , updateAddress);
 
 // ✅ Delete address by address ID
-router.delete("/deleteAddress/:id" ,auth ,isUser ,deleteAddress);
+router.delete("/deleteAddress/:id" ,auth  ,deleteAddress);
 
 module.exports = router;
